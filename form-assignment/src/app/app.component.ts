e13207fb8ba36639c47de4367294a98149cadf4b
Richard <gdi_richard@yahoo.com>
2019-07-12 13:59:10 -0700

    Mid-day work

--- form-assignment/src/app/app.component.ts
+++ form-assignment/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 	title = 'form-assignment';
 
 	signUpForm: FormGroup;
@@ -14,27 +14,26 @@ export class AppComponent {
 	ngOnInit(){
 	
 		this.signUpForm = new FormGroup({
-			'userName': new FormControl(null, Validators.required),
-			'userEmail': new FormControl(null, Validators.required)
+			"userName": new FormControl(null,Validators.required),
 		});
 
+		// Subscribe to status change
 		this.signUpForm.statusChanges.subscribe(
 			(status) => console.log(status)
 		);
 
 		// Update/patch form values
 		this.signUpForm.patchValue({
-			'userName': 'name',
-			'userEmail': 'email'
+			'userName': 'name'
 		});
 	}
 
 	onSubmit() {
 		console.log(this.signUpForm);
 		console.log(this.signUpForm.value);
-	  }
+	}
 	
-	  onReset() {
+	onReset() {
 		this.signUpForm.reset();
 	}
 }
