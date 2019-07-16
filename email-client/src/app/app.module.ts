e5416a7c4098825e5a3a7db74ccaef2e5bb12d5b
zerocnc <gdi_richard@yahoo.com>
2019-07-15 23:44:09 -0700

    Some Work done

---
+++ email-client/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { MessageFormComponent } from './message-form/message-form.component';
+import { EmailFormComponent } from './email-form/email-form.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path: '', component: LoginFormComponent},
+  { path: 'signup', component: SignupFormComponent },
+  { path: '**', component: PageNotFoundComponent }
+  ];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginFormComponent,
+    SignupFormComponent,
+    MessageFormComponent,
+    EmailFormComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ email-client/src/app/login-form/login-form.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.css']
+})
+export class LoginFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
